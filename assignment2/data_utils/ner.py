36883a0379ce6ffe9b1cc5f15296a9cb3c22adf2
tensilick <tensilick@proton.me>
2021-09-16 13:49:20 +0000

    minor editing

--- assignment2/data_utils/ner.py
+++ assignment2/data_utils/ner.py
@@ -9,4 +9,6 @@ from numpy import *
 def load_wv(vocabfile, wvfile):
     wv = loadtxt(wvfile, dtype=float)
     with open(vocabfile) as fd:
-        words = [line.strip() for line in fd
+        words = [line.strip() for line in fd]
+    num_to_word = dict(enumerate(words))
+    word_to_num
