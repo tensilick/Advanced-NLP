df3c0c68d9709e62f4936406ba70e5a2c1232d8f
tensilick <tensilick@proton.me>
2021-10-01 08:58:23 +0000

    RichText2 test suite:

--- assignment2/data_utils/ner.py
+++ assignment2/data_utils/ner.py
@@ -19,4 +19,4 @@ def save_predictions(y, filename):
     """Save predictions, one per line."""
     with open(filename, 'w') as fd:
         fd.write("\n".join(map(str, y)))
-        fd.write(
+        fd.write("\n")
---
+++ assignment2/data_utils/utils.py
@@ -0,0 +1,37 @@
+
+import sys, os, re, json
+import itertools
+from collections import Counter
+import time
+from numpy import *
+
+import pandas as pd
+
+
+def invert_dict(d):
+    return {v:k for k,v in d.iteritems()}
+
+def flatten1(lst):
+    return list(itertools.chain.from_iterable(lst))
+
+def load_wv_pandas(fname):
+    return pd.read_hdf(fname, 'data')
+
+def extract_wv(df):
+    num_to_word = dict(enumerate(df.index))
+    word_to_num = invert_dict(num_to_word)
+    wv = df.as_matrix()
+    return wv, word_to_num, num_to_word
+
+def canonicalize_digits(word):
+    if any([c.isalpha() for c in word]): return word
+    word = re.sub("\d", "DG", word)
+    if word.startswith("DG"):
+        word = word.replace(",", "") # remove thousands separator
+    return word
+
+def canonicalize_word(word, wordset=None, digits=True):
+    word = word.lower()
+    if digits:
+        if (wordset != None) and (word in wordset): return word
+        word = canonicalize_digits(word) # try to canonicalize numbers
