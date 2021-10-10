2f24d3653bafe1af1b94f550205058eb4a304811
tensilick <tensilick@proton.me>
2021-10-10 12:42:31 +0000

    Conflict merge

--- assignment2/nerwindow.py
+++ assignment2/nerwindow.py
@@ -74,3 +74,59 @@ class WindowMLP(NNBase):
         # any other initialization you need
 
         self.sparams.wv = wv.copy()
+        self.params.W  = random_weight_matrix(param_dims["W"][0],param_dims["W"][1])
+        self.params.U  = random_weight_matrix(param_dims["U"][0],param_dims["U"][1])
+
+        #done automatically
+        #self.params.b1 = zeros(param_dims["b1"])
+        #self.params.b2 = zeros(param_dims["b2"])
+        
+        #print "W  shape: %s" % (self.params.W.shape,)
+        #print "b1 shape: %s" % (self.params.b1.shape,)
+        #print "U  shape: %s" % (self.params.U.shape,)
+        #print "b2 shape: %s" % (self.params.b2.shape,)
+
+        #### END YOUR CODE ####
+
+    def _acc_grads(self, window, label):
+        """
+        Accumulate gradients, given a training point
+        (window, label) of the format
+
+        window = [x_{i-1} x_{i} x_{i+1}] # three ints
+        label = {0,1,2,3,4} # single int, gives class
+
+        Your code should update self.grads and self.sgrads,
+        in order for gradient_check and training to work.
+
+        So, for example:
+        self.grads.U += (your gradient dJ/dU)
+        self.sgrads.L[i] = (gradient dJ/dL[i]) # this adds an update for that index
+        """
+        #### YOUR CODE HERE ####
+
+        onehot_vecs = expand_dims(self.sparams.L[window,:].flatten(),axis=0)
+
+        #print "onehot_vecs.shape: %s " % (onehot_vecs.shape,)
+
+        ##
+        # Forward propagation
+        a1 = self.params.W.dot(onehot_vecs.T).T + self.params.b1
+        s  = sigmoid( 2.0 * a1 )
+        h  = 2.0 * s - 1.0
+        a2 = self.params.U.dot(h.T).T + self.params.b2
+        y_hat = softmax( a2 ) 
+
+        ##
+        # Backpropagation
+        t = zeros( y_hat.shape )
+        t[:,label] = 1
+
+        delta_out = y_hat - t
+
+        self.grads.U  += h.T.dot(delta_out).T + self.lreg * self.params.U
+
+        #print "delta_out  shape: %s" % (delta_out.shape,)
+
+        self.grads.b2 += delta_out.flatten()
+        #print "self.grads.b2.shape: %s " % (self.grads.b2.shape,)
