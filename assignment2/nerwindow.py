cd9c6813cfb85123b4ed2471c69ce11bbd417546
tensilick <tensilick@proton.me>
2021-10-10 06:10:46 +0000

    add domain

--- assignment2/nerwindow.py
+++ assignment2/nerwindow.py
@@ -20,3 +20,57 @@ def eval_performance(y_true, y_pred, tagnames):
     print "Mean F1:         %.02f%%" % (100*sum(f1[1:] * support[1:])/sum(support[1:]))
 
 ##
+# Implement this!
+##
+class WindowMLP(NNBase):
+    """Single hidden layer, plus representation learning."""
+
+    def __init__(self, wv, windowsize=3,
+                 dims=[None, 100, 5],
+                 reg=0.001, alpha=0.01, rseed=10):
+        """
+        Initialize classifier model.
+
+        Arguments:
+        wv : initial word vectors (array |V| x n)
+            note that this is the transpose of the n x |V| matrix L
+            described in the handout; you'll want to keep it in
+            this |V| x n form for efficiency reasons, since numpy
+            stores matrix rows continguously.
+        windowsize : int, size of context window
+        dims : dimensions of [input, hidden, output]
+            input dimension can be computed from wv.shape
+        reg : regularization strength (lambda)
+        alpha : default learning rate
+        rseed : random initialization seed
+
+        |V| = Size of vocabulary
+        n   = length of our word vectors
+        """
+
+        # Set regularization
+        self.lreg = float(reg)
+        self.alpha = alpha # default training rate
+
+        dims[0] = windowsize * wv.shape[1] # input dimension
+
+        print "input size:  %d" % dims[0]
+        print "hidden size: %d" % dims[1]
+        print "output size: %d" % dims[2]
+
+        param_dims = dict(W=(dims[1], dims[0]),
+                          b1=(dims[1],),
+                          U=(dims[2], dims[1]),
+                          b2=(dims[2],),
+                          )
+        param_dims_sparse = dict(L=wv.shape)
+
+        # initialize parameters: don't change this line
+        NNBase.__init__(self, param_dims, param_dims_sparse)
+
+        random.seed(rseed) # be sure to seed this for repeatability!
+        #### YOUR CODE HERE ####
+
+        # any other initialization you need
+
+        self.sparams.wv = wv.copy()
