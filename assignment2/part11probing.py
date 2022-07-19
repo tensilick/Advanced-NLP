13bdf29fd07354d83f5b8ad6e76784b4d41ebe74
tensilick <tensilick@proton.me>
2022-07-19 21:27:24 +0000

    readme note

--- assignment2/part11probing.py
+++ assignment2/part11probing.py
@@ -48,3 +48,63 @@ def part_b(clf, num_to_word, num_to_tag, verbose=True):
     """
     Code for 1.1 part (b):
     Model Output, Center Word
+
+    clf: instance of WindowMLP,
+            trained on data
+    num_to_word: dict {int:string}
+
+    You need to create:
+    - topscores : list of 5 lists of 10 probability scores (float)
+    - topwords  : list of 5 lists of 10 words (string)
+    where indices 0,1,2,3,4 correspond to num_to_tag, i.e.
+    tagnames = ["O", "LOC", "MISC", "ORG", "PER"]
+    """
+    #### YOUR CODE HERE ####
+
+
+
+
+
+
+    #### END YOUR CODE ####
+    # topscores[i]: list of floats
+    # topwords[i]: list of words
+    if verbose == True:
+        for i in range(1,5):
+            print "Output neuron %d: %s" % (i, num_to_tag[i])
+            print_scores(topscores[i], topwords[i])
+            print ""
+
+    return topscores, topwords
+
+
+def part_c(clf, num_to_word, num_to_tag, verbose=True):
+    """
+    Code for 1.1 part (c):
+    Model Output, Preceding Word
+
+    clf: instance of WindowMLP,
+            trained on data
+    num_to_word: dict {int:string}
+
+    You need to create:
+    - topscores : list of 5 lists of 10 probability scores (float)
+    - topwords  : list of 5 lists of 10 words (string)
+    where indices 0,1,2,3,4 correspond to num_to_tag, i.e.
+    tagnames = ["O", "LOC", "MISC", "ORG", "PER"]
+    """
+    #### YOUR CODE HERE ####
+
+
+
+
+
+
+    #### END YOUR CODE ####
+    # topscores[i]: list of floats
+    # topwords[i]: list of words
+    if verbose == True:
+        for i in range(1,5):
+            print "Output neuron %d: %s" % (i, num_to_tag[i])
+            print_scores(topscores[i], topwords[i])
+            print ""
