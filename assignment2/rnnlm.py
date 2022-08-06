829dfea8c2cb087cf51095152b2025fcebbe298a
tensilick <tensilick@proton.me>
2022-08-06 16:19:52 +0000

    black

---
+++ assignment2/rnnlm.py
@@ -0,0 +1,38 @@
+
+from numpy import *
+import itertools
+import time
+import sys
+
+# Import NN utils
+from nn.base import NNBase
+from nn.math import softmax, sigmoid, make_onehot
+from nn.math import MultinomialSampler, multinomial_sample
+from misc import random_weight_matrix, sigmoid_grad, softmax
+
+
+class RNNLM(NNBase):
+    """
+    Implements an RNN language model of the form:
+    h(t) = sigmoid(H * h(t-1) + L[x(t)])
+    y(t) = softmax(U * h(t))
+    where y(t) predicts the next word in the sequence
+
+    U = |V| * dim(h) as output vectors
+    L = |V| * dim(h) as input vectors
+
+    You should initialize each U[i,j] and L[i,j]
+    as Gaussian noise with mean 0 and variance 0.1
+
+    Arguments:
+        L0 : initial input word vectors
+        U0 : initial output word vectors
+        alpha : default learning rate
+        bptt : number of backprop timesteps
+    """
+
+    def __init__(self, L0, U0=None,
+                 alpha=0.005, rseed=10, bptt=1):
+
+        self.hdim = L0.shape[1] # word vector dimensions
+        self.vdim = L0.shape[0] # vocab size
