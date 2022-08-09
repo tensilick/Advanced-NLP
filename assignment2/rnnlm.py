8ef2cceef19505bd15367e3e1a7c8a76db3f0009
tensilick <tensilick@proton.me>
2022-08-09 16:45:53 +0000

    added license badge

--- assignment2/rnnlm.py
+++ assignment2/rnnlm.py
@@ -36,3 +36,52 @@ class RNNLM(NNBase):
 
         self.hdim = L0.shape[1] # word vector dimensions
         self.vdim = L0.shape[0] # vocab size
+        param_dims = dict(H = (self.hdim, self.hdim),
+                          U = L0.shape)
+        # note that only L gets sparse updates
+        param_dims_sparse = dict(L = L0.shape)
+        NNBase.__init__(self, param_dims, param_dims_sparse)
+
+        #### YOUR CODE HERE ####
+
+        # Initialize word vectors
+        # either copy the passed L0 and U0 (and initialize in your notebook)
+        # or initialize with gaussian noise here
+
+        if U0 is None:
+            self.params.U = random.normal(0,0.1,*param_dims["U"])
+        else:
+            self.params.U = U0.copy()
+
+        if L0 is None:
+            self.sparams.L = random.normal(0,0.1,*param_dims["L"])
+        else:
+            self.sparams.L = L0.copy()
+
+        # Initialize H matrix, as with W and U in part 1
+
+        self.params.H = random_weight_matrix(*param_dims["H"])
+
+        self.rseed = rseed
+        self.bptt  = bptt
+        self.alpha = alpha
+
+        #### END YOUR CODE ####
+
+
+    def _acc_grads(self, xs, ys):
+        """
+        Accumulate gradients, given a pair of training sequences:
+        xs = [<indices>] # input words
+        ys = [<indices>] # output words (to predict)
+
+        Your code should update self.grads and self.sgrads,
+        in order for gradient_check and training to work.
+
+        So, for example:
+        self.grads.H += (your gradient dJ/dH)
+        self.sgrads.L[i] = (gradient dJ/dL[i]) # update row
+
+        Per the handout, you should:
+            - make predictions by running forward in time
+                through the entire input sequence
