55cf6f68209f8bfc2387fd2c8458418bc1a486ea
tensilick <tensilick@proton.me>
2022-08-18 11:43:31 +0000

    Update results

--- assignment3/rnn.py
+++ assignment3/rnn.py
@@ -10,3 +10,43 @@ import collections
 # tip: insert pdb.set_trace() in places where you are unsure whats going on
 
 class RNN:
+
+    def __init__(self,wvecDim,outputDim,numWords,mbSize=30,rho=1e-4):
+        self.wvecDim = wvecDim
+        self.outputDim = outputDim
+        self.numWords = numWords
+        self.mbSize = mbSize
+        self.defaultVec = lambda : np.zeros((wvecDim,))
+        self.rho = rho
+
+    def initParams(self):
+        np.random.seed(12341)
+
+        # Word vectors
+        self.L = 0.01*np.random.randn(self.wvecDim,self.numWords)
+
+        # Hidden layer parameters
+        self.W = 0.01*np.random.randn(self.wvecDim,2*self.wvecDim)
+        self.b = np.zeros((self.wvecDim))
+
+        # Softmax weights
+        self.Ws = 0.01*np.random.randn(self.outputDim,self.wvecDim) # note this is " U " in the notes and the handout.. there is a reason for the change in notation
+        self.bs = np.zeros((self.outputDim))
+
+        self.stack = [self.L, self.W, self.b, self.Ws, self.bs]
+
+        # Gradients
+        self.dW = np.empty(self.W.shape)
+        self.db = np.empty((self.wvecDim))
+        self.dWs = np.empty(self.Ws.shape)
+        self.dbs = np.empty((self.outputDim))
+
+    def costAndGrad(self,mbdata,test=False):
+        """
+        Each datum in the minibatch is a tree.
+        Forward prop each tree.
+        Backprop each tree.
+        Returns
+           cost
+           Gradient w.r.t. W, Ws, b, bs
+           Gradient w.r.t. L in sparse form.
