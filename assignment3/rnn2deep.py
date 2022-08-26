0906e4d66b6e65ed4275158dc204180b7a7e256c
tensilick <tensilick@proton.me>
2022-08-26 07:41:53 +0000

    cmake fixes

--- assignment3/rnn2deep.py
+++ assignment3/rnn2deep.py
@@ -25,3 +25,54 @@ class RNN2:
 
     def initParams(self):
         np.random.seed(12341)
+
+        # Word vectors
+        self.L = 0.01*np.random.randn(self.wvecDim,self.numWords)
+
+        # Hidden activation weights for layer 1
+        self.W1 = 0.01*np.random.randn(self.wvecDim,2*self.wvecDim)
+        self.b1 = np.zeros((self.wvecDim))
+
+        # Hidden activation weights for layer 2
+        self.W2 = 0.01*np.random.randn(self.middleDim,self.wvecDim)
+        self.b2 = np.zeros((self.middleDim))
+
+        # Softmax weights
+        self.Ws = 0.01*np.random.randn(self.outputDim,self.middleDim) # note this is " U " in the notes and the handout.. there is a reason for the change in notation
+        self.bs = np.zeros((self.outputDim))
+
+        self.stack = [self.L, self.W1, self.b1, self.W2, self.b2, self.Ws, self.bs]
+
+        # Gradients
+        self.dW1 = np.empty(self.W1.shape)
+        self.db1 = np.empty((self.wvecDim))
+
+        self.dW2 = np.empty(self.W2.shape)
+        self.db2 = np.empty((self.middleDim))
+
+        self.dWs = np.empty(self.Ws.shape)
+        self.dbs = np.empty((self.outputDim))
+
+    def costAndGrad(self,mbdata,test=False):
+        """
+        Each datum in the minibatch is a tree.
+        Forward prop each tree.
+        Backprop each tree.
+        Returns
+           cost
+           Gradient w.r.t. W1, W2, Ws, b1, b2, bs
+           Gradient w.r.t. L in sparse form.
+
+        or if in test mode
+        Returns
+           cost, correctArray, guessArray, total
+        """
+        cost = 0.0
+        correct = []
+        guess = []
+        total = 0.0
+
+        self.L, self.W1, self.b1, self.W2, self.b2, self.Ws, self.bs = self.stack
+        # Zero gradients
+        self.dW1[:] = 0
+        self.db1[:] = 0
