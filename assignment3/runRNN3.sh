68860138e1e8be32e50f76ad8fff3b4d23d4bfd0
tensilick <tensilick@proton.me>
2022-09-29 20:19:29 +0000

    Readme: typo

--- assignment3/runRNN3.sh
+++ assignment3/runRNN3.sh
@@ -14,10 +14,11 @@ step=1e-2
 # for RNN2 only, otherwise doesnt matter
 middleDim=30
 
-model="RNN2" #either RNN, RNN2, RNN3, RNTN, or DCNN
+model="RNN3" #either RNN, RNN2, RNN3, RNTN, or DCNN
 
 #missing 5 on purpose, already ran
-middleDimBatch=("5" "15" "25" "30" "35" "45")
+#middleDimBatch=("5" "15" "25" "30" "35" "45")
+middleDimBatch=("30")
 wvecDim=30
 
 for middleDim in "${middleDimBatch[@]}"
@@ -29,12 +30,3 @@ if [ "$model" == "RNN2" ]; then
     outfile="models/${model}_wvecDim_${wvecDim}_middleDim_${middleDim}_step_${step}_2.bin"
 else
     outfile="models/${model}_wvecDim_${wvecDim}_step_${step}_2.bin"
-fi
-
-
-echo $outfile
-
-
-python runNNet.py --step $step --epochs $epochs --outFile $outfile \
-                --middleDim $middleDim --outputDim 5 --wvecDim $wvecDim --model $model
-done
