5a9c8883e5b9fa797996592cf5b1f1cb8e7ea9f1
tensilick <tensilick@proton.me>
2022-12-24 21:01:06 +0000

    debug cuda dfs

--- assignment3/test.sh
+++ assignment3/test.sh
@@ -4,4 +4,7 @@
 set -x
 
 
-infile="models/RNN_wvecDim_25_step_1e-2_2.bin" # the pickled neural 
+infile="models/RNN_wvecDim_25_step_1e-2_2.bin" # the pickled neural network
+model="RNN" # the neural network type
+
+echo $i
